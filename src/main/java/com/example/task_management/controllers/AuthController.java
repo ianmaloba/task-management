2da18501abb009604c77daa151f659971426b445
Ian Maloba <ianmalobamwakha@gmail.com>
2024-12-07 02:55:11 +0300

    Added methods for signup and login functionality.

---
+++ src/main/java/com/example/task_management/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.task_management.controllers;
+
+import com.example.task_management.models.User;
+import com.example.task_management.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/signup")
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String registerUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
